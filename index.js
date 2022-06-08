72e06bb61ed0bdc57f7e52467e2ad87d6fe3a34b
Brun0-marinh0 <bcaldasmarinhojklb@gmail.com>
2022-06-07 21:51:40 -0400

    tipagens

--- index.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById('numb1');
-var input2 = document.getElementById('numb1');
-var button = document.getElementById('button');
-function sum(a, b) {
-    return a + b;
-}
-button.addEventListener("click", function () {
-    console.log(Number(input1.value), Number(input2.value));
-});
---
+++ types.ts
@@ -0,0 +1,51 @@
+// boolean
+let isOpen: boolean
+isOpen = true
+
+// string
+let message: string
+message = `foo ${isOpen}`
+
+// number (int, float, hex, binary)
+let total: number
+total = 0xff0
+
+// array (type[])
+let items: string[]
+items = ["oi", "tchau"]
+
+let values: Array<number>
+values = [1, 2, 3]
+
+// tuple
+let title: [number, string]
+title = [1, "oi"]
+
+// enum para por nomes mais claros em valores...um apelido
+enum Colors {
+    white = '#fff',
+    black = '#000'
+}
+
+// any ;-;
+let coisa: any
+coisa = [1]
+
+// void(vazio)
+function logger(){
+    console.log('foo')
+}
+
+// null / undefined
+type bla = string | undefined
+
+// never 
+function error(): never{
+    throw new Error('error')
+}
+
+// object
+let cart: object
+cart = {
+    key: 'fi'
+}
