38af3233e443498328eb0551e11faff17d0c4df0
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-06-10 09:38:34 -0300

    estendendo uma classe

--- classes.ts
+++ classes.ts
@@ -11,7 +11,21 @@ class UserAccount {
         console.log(`The player ${this.name} is ${this.age} years old.`)
     }
 }
+class CharAccount extends UserAccount{
+    nickname: string
+    level: number
+
+    constructor(nickname: string, level: number, name: string, age: number){
+        super(name, age)
+
+        this.nickname = nickname
+        this.level = level
+    }
+}
 
 const bruno = new UserAccount("Bruno", 22)
 
 bruno.logDetails()
+
+const jkl = new CharAccount("JKL_br_", 12, "Bruno Marinho", 22)
+jkl.logDetails()
